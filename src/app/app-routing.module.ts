1caf17fcb2988647b3f2dc8351aee29d3692e62e
habib5k <benjdidiahabib15@gmail.com>
2023-05-05 12:07:27 +0100

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CatComponent } from './cat/cat.component';
-
+import { CategoriesComponent } from './categories/categories.component';
 const routes: Routes = [
-  { path: 'cat', component: CatComponent }
+  { path: 'cat', component: CatComponent },
+  {path :'categories' ,component: CategoriesComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ProjetComponent } from './projet/projet.component';
 import { CatComponent } from './cat/cat.component';
+import { CategoriesComponent } from './categories/categories.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ProjetComponent,
-    CatComponent
+    CatComponent,
+    CategoriesComponent
   ],
   imports: [
     BrowserModule,
