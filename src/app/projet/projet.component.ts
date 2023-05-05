1830f1eaba020f5ccc071527d295ec9fcba103fe
habib5k <benjdidiahabib15@gmail.com>
2023-05-05 12:04:08 +0100

    commit

--- src/app/projet/projet.component.ts
+++ src/app/projet/projet.component.ts
@@ -9,9 +9,17 @@ import { Router } from '@angular/router';
 })
 export class ProjetComponent {
 public should_open =false ;
+
+public should_open2 =false ;
   constructor(private router: Router) {}
 
   goToCat() {
   this.should_open=true ;
   }
+
+  goToCategorie(){
+    this.router.navigate(['/categories']);
+
+  }
+
 }
